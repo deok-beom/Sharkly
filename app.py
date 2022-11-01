c238099cc8bd59fbf1ecc460c16c2a1882a373d8
klaXXi <baek940dog@gmail.com>
2022-11-01 13:00:52 +0900

    20221101

--- app.py
+++ app.py
@@ -15,22 +15,49 @@ def bsb_page():
     return render_template('bsb.html')
 
 @app.route("/bsb", methods=["POST"])
-def guestbook_post():
+def bsb_guestbook_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
+    password_receive = request.form['password_give']
 
     doc = {
         'name': name_receive,
-        'comment': comment_receive
+        'comment': comment_receive,
+        'password': password_receive
     }
     db.guestbook.insert_one(doc)
-
-    return jsonify({'msg':'댓글 남기기 완료!'})
+    return jsonify({'msg':"성공"})
 
 @app.route("/bsb", methods=["GET"])
-def guestbook_get():
+def bsb_guestbook_get():
     guestbook_list = list(db.guestbook.find({}, {'_id':False}))
     return jsonify({'guestbook':guestbook_list})
+
+@app.route("/bsb", methods=["PATCH"])
+def bsb_guestbook_patch():
+    name_receive = request.form['name_give'].strip()
+    comment_receive = request.form['comment_give']
+    password_receive = request.form['password_give']
+
+    commentInfo = db.guestbook.find_one({'name': name_receive})
+    if commentInfo['password'] == password_receive:
+        db.guestbook.update_one({'name': name_receive}, {'$set': {'comment': comment_receive}})
+        return jsonify({'msg':"성공"})
+    else:
+        return jsonify({'msg':"비밀번호가 일치하지 않습니다"})
+
+@app.route("/bsb", methods=["DELETE"])
+def bsb_guestbook_delete():
+    name_receive = request.form['name_give'].strip()
+    password_receive = request.form['password_give']
+
+    commentInfo = db.guestbook.find_one({'name': name_receive})
+    if commentInfo['password'] == password_receive:
+        db.guestbook.delete_one({'name': name_receive})
+        return jsonify({'msg':"성공"})
+    else:
+        return jsonify({'msg':"비밀번호가 일치하지 않습니다"})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
